124d37574362007acfbbbba29561f90103188ed2
Reed Piernock <contact@reedcodes.com>
2018-12-05 01:29:17 +0000

    initial page for Gauge Pride

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require( 'gulp' ),
+    concat = require( 'gulp-concat' ),
+    compass = require( 'gulp-compass' );
+
+function compassBuild() {
+    return gulp.src( 'scss/**/*.scss' )
+        .pipe( compass({
+            css: 'css',
+            sass: 'scss',
+            image: 'img',
+            style: 'compressed'
+        }) )
+        .pipe( gulp.dest( 'css' ) );
+}
+
+function watchBuild() {
+  gulp.watch( 'scss/**/*.scss', compassBuild );
+}
+
+gulp.task( 'default', compassBuild );
+gulp.task( 'watch', watchBuild );
